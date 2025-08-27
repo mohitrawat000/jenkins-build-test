7326fccdc08237e75ec8c9d120681df239785191
mohitrawat000 <mohitrawat000987@gmail.com>
2025-08-27 10:45:42 +0530

    1st code commit

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
