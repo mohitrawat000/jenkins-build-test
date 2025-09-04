258ce35e348b119ba5ddf18d73c781be934f86cb
Mohit Rawat <mohitrawat000987@gmail.com>
2025-09-04 12:26:49 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins!");
+        System.out.println("Hello Mohit RAWAT Jenkins WORK !");
     }
 
     public int add(int a, int b) {
